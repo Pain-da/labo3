821f194761b4e06bd903ae6faf71360625d2f093
Stefano <stefano.pontarolo@heig-vd.ch>
2020-10-27 14:23:34 +0100

    add index

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier : <nom du fichier>.<xxx> (xxx = h ou cpp)
+Auteur(s) : <Dimitri> <De Bleser>, <Léo> <Meli>, <Stefano> <Pontarolo>
+Date creation : <27.10.2020>
+Description : <à compléter>
+Remarque(s) : <à compléter>
+Compilateur : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+*/
+#include <cstdlib>
 #include <iostream>
-
+using namespace std;
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+
+ return EXIT_SUCCESS;
 }
